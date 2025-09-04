c8a6aea999153b957202b0ba18f81fd43c9924cb
StalkerKindred <MMMeriaddd@gmail.com>
2025-09-04 05:55:01 +0200

    Several functions for models

--- mainsite/Insurence_Web/models.py
+++ mainsite/Insurence_Web/models.py
@@ -12,6 +12,12 @@ class InsurerEmployees(models.Model):
         email = models.CharField(max_length=100)
         password = models.CharField(max_length=100)
 
+        def __str__(self):
+                return  self.first_name + f" {self.last_name}" + f" - ID: {self.insurer_id}"
+        
+        def contacts(self):
+                return (f"Email: {self.email}")
+        
 class InsuredPersons(models.Model):
         #Keys
         insured_id = models.PositiveIntegerField(primary_key=True)
@@ -24,11 +30,18 @@ class InsuredPersons(models.Model):
         email = models.CharField(max_length=100)
         password = models.CharField(max_length=100) 
 
+        def __str__(self):
+                return  self.first_name + f" {self.last_name}" + f" - ID: {self.insured_id}"
+        
+        def contacts(self):
+                return (f"""Email: {self.email}
+                        Phone: {self.phone_number}""")
+        
 class InsurenceQuestionare(models.Model):
         #Keys
         insurence_questionare_id = models.PositiveIntegerField(primary_key=True)
-        insured_fk = models.ForeignKey(InsuredPersons, on_delete=models.CASCADE, related_name="insured_person")
-        insurer_fk = models.ForeignKey(InsuredPersons, on_delete=models.PROTECT, related_name="insurer_employee")
+        insured = models.ForeignKey(InsuredPersons, on_delete=models.CASCADE, related_name="insured_person")
+        insurer = models.ForeignKey(InsuredPersons, on_delete=models.PROTECT, related_name="insurer_employee")
         #Default 0 means No
         #Data
         question_1_answer = models.IntegerField(default=0)
@@ -42,6 +55,14 @@ class InsurenceQuestionare(models.Model):
         question_9_answer = models.IntegerField(default=0)
         question_10_answer = models.IntegerField(default=0)
 
+        def __str__(self):
+                return (f"Insurence Questionare ID: {self.insurence_questionare_id}")
+        
+        def more_info(self):
+                return (f"""Insurence Questionare ID: {self.insurence_questionare_id} 
+                        Filled by {self.insured}
+                        Signed by {self.insurer}""")
+        
 class Insurence(models.Model):
         #Keys
         insurence_id = models.PositiveIntegerField(primary_key=True)
@@ -53,4 +74,19 @@ class Insurence(models.Model):
         insurence_end = models.DateTimeField()
         insurence_type = models.CharField(max_length=100)
         insurence_monthly_payment = models.PositiveIntegerField()
-        insurence_loss_payment = models.PositiveIntegerField()
+        insurence_claims_payment = models.PositiveIntegerField()
+
+        def __str__(self):
+                return (f"Insurence ID: {self.insurence_id}")
+        
+        def more_info(self):
+                return (f"""Insurence  ID: {self.insurence_id} 
+                        Insurence Type {self.insurence_type}
+                        Filled by {self.insured}
+                        Signed by {self.insurer}""")
+        
+        def payments(self):
+                return(f"""Start {self.insurence_start}
+                        End {self.insurence_end}
+                        Monthly Payment {self.insurence_monthly_payment}
+                        Claims Payment {self.insurence_claims_payment}""")
