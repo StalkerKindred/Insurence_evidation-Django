6d9f5cf296b243aa5e904faed7044d8bd3de2b18
StalkerKindred <MMMeriaddd@gmail.com>
2025-10-04 14:39:04 +0200

    Integrated Bootstrap 5.3, Working on placeholder pages.

--- mainsite/Insurence_Web/views.py
+++ mainsite/Insurence_Web/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import InsuredPersons, InsurerEmployees, Insurence, InsurenceQuestionare
+
 def index(request):
-    return HttpResponse("This is the begining")
+    context = "This is truly the begining" 
+    return render(request, "Insurence_Web/base.html", {"message": context})
 
 def insured_detail(request, id):
-    detail = InsuredPersons.objects.get(insured_id=id)
+    detail = get_object_or_404(InsuredPersons, pk=id)
     return HttpResponse(str(detail) +" "+ detail.contacts())
 
 def insurence_detail(request, id):
-    detail = Insurence.objects.get(insurence_id=id)
+    detail = get_object_or_404(Insurence, pk=id)
     return HttpResponse(str(detail) + " "+ detail.more_info() + " " + detail.payments())
 
 def questionare_detail(request, id):
-    detail = InsurenceQuestionare.objects.get(insurence_questionare_id=id)
+    detail = get_object_or_404(InsurenceQuestionare, pk=id)
     return HttpResponse(detail.more_info())
